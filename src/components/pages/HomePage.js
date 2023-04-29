21aa1d2a2c8e435776d3e548109ae435b564a765
a-atchuta-ram <97715909+a-atchuta-ram@users.noreply.github.com>
2023-04-29 08:08:46 +0530

    New commit @8:08

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,7 +4,15 @@ import { Link } from 'react-router-dom'
 export default function HomePage() {
     return (
         <div className="text-center">
-            <h1 className="main-title home-page-title">welcome to our app</h1>
+            <h1>User's Page </h1>
+            <p style={{fontSize:30}}> User Profile</p>
+            <p style={{fontSize:30}}> Orders --</p>
+            <Link to="/">
+                <button className="primary-button">Buy</button>
+            </Link>
+            <Link to="/">
+                <button className="primary-button">Add Books</button>
+            </Link>
             <Link to="/">
                 <button className="primary-button">Log out</button>
             </Link>
